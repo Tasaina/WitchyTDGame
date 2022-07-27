ac3eab115bd4e61a12c9128677ca41b4412131e2
MoongazingCat <Philine.O@gmx.de>
2022-07-27 20:06:38 +0200

    Added Bullet prefab

--- Assets/Project/Scripts/Bullet.cs
+++ Assets/Project/Scripts/Bullet.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
-    public float attackRange;
+    public Vector3 turretPosition;
+    private Vector3 targetPosition;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
