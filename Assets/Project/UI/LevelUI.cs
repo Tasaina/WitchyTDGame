7ed08c63793beb737df00cb5bfbcc0a577cc5c6e
MoongazingCat <Philine.O@gmx.de>
2022-08-21 16:20:03 +0200

    Added Wave indicator

--- Assets/Project/UI/LevelUI.cs
+++ Assets/Project/UI/LevelUI.cs
@@ -10,14 +10,23 @@ public class LevelUI : MonoBehaviour
 {
     public Turret turretToSpawn;
     private TextMeshProUGUI essenceText;
+    private TextMeshProUGUI waveText;
     void Start()
     {
         GetComponentInChildren<Button>().onClick.AddListener(SpawnTowerClicked);
         essenceText = GetComponentsInChildren<TextMeshProUGUI>().First(t=>t.name=="EssenceText");
+        waveText = GetComponentsInChildren<TextMeshProUGUI>().First(t => t.name == "WaveText");
+        
     }
     private void Update()
     {
         essenceText.text = $"Essence: {GameManager.Instance.LevelManager.essence}";
+        UpdateWave();//use WaveChange within WaveManager instead of this Update?
+    }
+    private void UpdateWave()
+    {
+        var waveManager = GameManager.Instance.WaveManager;
+        waveText.text = $"Wave {waveManager.currentWave} / {waveManager.maxWaves}";
     }
     private void SpawnTowerClicked()
     {
